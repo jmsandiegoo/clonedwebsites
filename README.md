5ff145a66482ef75b746050155b256c78ad76efa
John Michael San Diego <jmsandiego0710@gmail.com>
2020-03-20 01:00:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clonedwebsites
+
+This repository consists of websites I copied from the official ones. Note: this is purely for practice and educational purposes.
---
+++ microsoft/microsoft.js
@@ -0,0 +1,6 @@
+
+let menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    document.querySelector(".main-menu").classList.toggle('show');
+})
